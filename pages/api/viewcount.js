ea81130a6e533c0e3b8dfb41078a89d017167528
Vladimir Sofronov <narviii@gmail.com>
2020-03-06 01:22:27 -0800

    ...

--- pages/api/viewcount.js
+++ pages/api/viewcount.js
@@ -2,19 +2,21 @@ import * as admin from 'firebase-admin';
 
 let serviceAccount = require('./apploan-535837fc3b12.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://apploan-b02b0.firebaseio.com"'
-});
+
 
 
 
 
 export default async function viewcount(req, res) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: 'https://apploan-b02b0.firebaseio.com"'
+      }).catch((err)=>res.send(err));
     const db = admin.firestore();
     const increment = admin.firestore.FieldValue.increment(1);
     db.collection('brand_views').doc(req.query.brandname).update({views:increment})
         .catch((err)=>{
+            res.send(err)
             console.log(err.code)
             if (err.code==5) db.collection('brand_views').doc(req.query.brandname).set({views:1})
         })
