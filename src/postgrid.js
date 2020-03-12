ec3d017ee87029ed2c8a01a618250f10cd0ce2a9
Vladimir Sofronov <narviii@gmail.com>
2020-03-12 00:10:01 -0700

    webp images

--- src/postgrid.js
+++ src/postgrid.js
@@ -209,7 +209,7 @@ function PostCard(props) {
 
                 >
                     <CardMedia
-                        image={props.entrie.fields.thumbnail ? props.entrie.fields.thumbnail.fields.file.url + '?w=1024' : 'https://via.placeholder.com/150'}
+                        image={props.entrie.fields.thumbnail ? props.entrie.fields.thumbnail.fields.file.url + '?w=1024' +'&fm=webp' : 'https://via.placeholder.com/150'}
                         className={classes.cardMedia}
                     >
                     </CardMedia>
