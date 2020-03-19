00c7755aa393a2b7e14c0a0d3cef49cf78bddf03
Vladimir Sofronov <narviii@gmail.com>
2020-03-18 22:30:44 -0700

    ...

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ const client = contentful.createClient({
 //UA-39274880-4 dev
 
 function initializeReactGAmain() {
-    ReactGA.initialize('UA-39274880-4');
+    ReactGA.initialize('UA-39274880-3');
 
 }
 
