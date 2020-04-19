3b71d7c40032a935df06a66ddef72bde25198ef2
Vladimir Sofronov <narviii@gmail.com>
2020-04-18 22:05:16 -0700

    ...

--- pages/brands/[brand].js
+++ pages/brands/[brand].js
@@ -4,25 +4,71 @@ import ReactGA from '../../src/reactga'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { Footer } from '../../src/footer';
 import { originList, tagList, sizeList } from '../../src/constants'
+import { Typography } from '@material-ui/core';
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper'
+import Link from '@material-ui/core/Link'
+import Box from '@material-ui/core/Box'
+import Chip from '@material-ui/core/Chip'
+import Avatar from '@material-ui/core/Avatar'
+import Divider from '@material-ui/core/Divider'
 
 
 
 function Brand(props) {
-    console.log(props)
     return (
         <React.Fragment>
-        <CssBaseline/>
-        <h1>{props.params.brand}</h1>
+            <CssBaseline />
+            <Container style={{ margin: '30px auto' }} maxWidth='md'>
+                <Paper style={{ padding: '20px' }}>
+                    <Box style={{ display: 'flex' }}>
+                        <Avatar style={{ width: '50px', height: '50px' }} alt={props.entrie.fields.title} src={props.entrie.fields.thumbnail ? props.entrie.fields.thumbnail.fields.file.url + '?w=1024' + '&fm=jpg' : 'https://via.placeholder.com/150'} />
+                        <Box style={{ margin: '0px 20px 0px 20px' }}>
+                            <Typography variant='h6'>
+                                {props.entrie.fields.title}
+                            </Typography>
+                            <Typography variant='subtitle2'>{props.entrie.fields.origin}</Typography>
+                        </Box>
+
+                        <Box style={{ margin: '0px 20px' }}>
+                            <Box display="flex" flexWrap="wrap" justifyContent="left">
+                                {props.entrie.fields.sizes ? props.entrie.fields.sizes.map(tag => (
+                                    <Chip key={tag} label={tag} style={{ margin: "10px" }} />
+                                )) : null}
+                                {props.entrie.fields.tags.map(tag => (
+                                    <Chip key={tag} label={tag} style={{ margin: "10px" }} />
+                                ))}
+
+                            </Box>
+
+                        </Box>
+                    </Box>
+                    <Divider style={{ margin: '10px' }} variant="middle" />
+                    <Box>
+                        <Typography variant="body2" color="textSecondary" gutterBottom component="p">
+                            {props.entrie.fields.desc}
+                        </Typography>
+
+                    </Box>
+
+                </Paper>
+            </Container>
         </React.Fragment>
     )
 }
 
 
 export async function getServerSideProps(context) {
+    console.log(context.params.brand)
+    let entries = await client.getEntries({
+        include: 1,
+        'fields.slug': context.params.brand,
+        'content_type': 'post',
+        limit: 1,
 
-
-
-    return { props: { params:context.params } }
+    })
+    console.log(entries)
+    return { props: { entrie: entries.items[0] } }
 }
 
 export default Brand
---
+++ pages/stockists.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import { client } from '../src/contentful'
+import ReactGA from '../src/reactga'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { Footer } from '../src/footer';
+import { originList, tagList, sizeList } from '../src/constants'
+import { Hero } from '../src/hero'
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardHeader from '@material-ui/core/CardHeader';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper'
+import { Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box'
+import Chip from '@material-ui/core/Chip'
+
+
+
+
+
+function StockCard(props) {
+    console.log(props.entrie)
+
+    const brands = props.entrie.fields.brands.map(item => (
+        <Chip style={{ margin: '10px 20px' }}
+            
+            label={item.fields.title}/>
+        
+    ))
+
+
+
+    return (
+        <Grid item xs={12}>
+
+            <Card style={{height:'100%'}}>
+                <CardHeader
+                    title={props.entrie.fields.name}
+                    subheader={props.entrie.fields.country}
+                    action={props.entrie.fields.format}
+                />
+
+                <CardContent>
+                    <Box style={{ display: 'flex', flexDirection: 'row',flexWrap: 'wrap', margin: '10px auto' }}>
+                        {brands}
+                    </Box>
+
+                </CardContent>
+            </Card>
+
+
+        </Grid>
+    )
+}
+
+function Stockists(props) {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Hero search={false} />
+            <Container style={{ margin: '30px auto' }} maxWidth='lg'>
+                <Grid direction="row" container spacing={4}>
+                    {props.entries.items.map(entrie => (
+                        <StockCard entrie={entrie} />
+                    ))}
+                </Grid>
+            </Container>
+
+            <Footer entries={props.stats} originList={originList} />
+
+        </React.Fragment>
+    )
+}
+
+
+
+export async function getServerSideProps(context) {
+    let entries = await client.getEntries({
+        include: 1,
+        'content_type': 'stockist',
+
+    })
+
+    const stats = await client.getEntries({
+        limit: 1
+    })
+
+
+    return { props: { entries: entries, stats: stats } }
+}
+
+export default Stockists;
