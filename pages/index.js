dfcafd98a1e36ec594c135c898bc06612e18d65a
narviii@gmail.com <narviii@gmail.com>
2021-01-21 20:05:35 -0800

    ...

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ import Head from 'next/head'
 import { HeadContent } from '../src/headcontent'
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
-import { Typography } from '@material-ui/core';
+import { SvgIcon, Typography } from '@material-ui/core';
 import Link from '@material-ui/core/Link'
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 
@@ -299,15 +299,17 @@ function MainPage(props) {
 
                 <Box p={3} flexWrap="wrap" display="flex" alignItems="center" bgcolor="info.main">
                     <Box flexGrow={0.5} />
-                    <MonetizationOnIcon fontSize="large" style={{ marginRight: '5px' }} />
+                    <SvgIcon style={{marginRight:"5px"}} fontSize="large">
+                        <path d="M19.83,7.5l-2.27-2.27c0.07-0.42,0.18-0.81,0.32-1.15C17.96,3.9,18,3.71,18,3.5C18,2.67,17.33,2,16.5,2 c-1.64,0-3.09,0.79-4,2l-5,0C4.46,4,2,6.46,2,9.5S4.5,21,4.5,21l5.5,0v-2h2v2l5.5,0l1.68-5.59L22,14.47V7.5H19.83z M13,9H8V7h5V9z M16,11c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1s1,0.45,1,1C17,10.55,16.55,11,16,11z" />
+                    </SvgIcon>
                     <Typography  >
                         New! Lingerie discounts live tracker. I scan quite a few lingerie websites for price changes(and discounts) so that you don't have to.
                        </Typography>
                     <Box flexGrow={0.5} />
-                    <Button style={{marginTop:"5px"}} href="/pricetrack" variant="contained" color="secondary">
+                    <Button style={{ marginTop: "5px" }} href="/pricetrack" variant="contained" color="secondary">
                         Show me
                        </Button>
-                       <Box flexGrow={0.5} />
+                    <Box flexGrow={0.5} />
 
                 </Box>
 
